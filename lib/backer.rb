d92c0b1d715eca3e591929ca723da14c7bef2f09
Ryan Schmelter <rschmelter33@gmail.com>
2017-12-08 18:49:32 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,6 @@
 class Backer
   attr_accessor :name, :backed_projects
-
+#I initially had @backed_projects set in initialize and not attr_accessor. and def backed_projects didn't work. Why?
   def initialize(name)
     @name = name
     @backed_projects = []
